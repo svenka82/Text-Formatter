aec039a1a4414562911b65fc0d9189ac6864d6b7
Richard Simpson <simpsric@gmail.com>
2018-03-13 13:02:55 -0700

    "removed unnecessary code"

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -451,31 +451,4 @@ public class MainWindow implements ActionListener {
 		mainFrame.getContentPane().setLayout(null);
 		mainFrame.setLocationRelativeTo(null);
 	}
-	
-	//private class VerifyPanelPopUp extends WindowAdapter
-	//{
-	//	JFrame f;
-	//	VerifyPanelPopUp(){
-	//		f = new JFrame();
-	//		f.addWindowListener(this);
-	//		f.setSize(300,300);
-	//		f.setLayout(null);
-	//		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-	//		f.setVisible(true);
-	//	}
-	//	public void windowClosing(WindowEvent e)
-	//	{
-	//		int a = JOptionPane.showConfirmDialog(f, "This will overwrite the file","Are you sure?");
-	//		if(a==JOptionPane.YES_OPTION)
-	//		{
-	//			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//			return;
-	//		}
-	//		else
-	//		{
-	//			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//			return;
-	//		}
-	//	}
-	//}
 }
