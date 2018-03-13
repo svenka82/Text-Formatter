3d2633c5cd685132365ffd2aeebf04144e4bdf52
svenka82 <37093392+svenka82@users.noreply.github.com>
2018-03-12 23:50:37 -0700

    Refactoring

--- src/GUI/Formatter.java
+++ src/GUI/Formatter.java
@@ -6,48 +6,48 @@ package GUI;
 
 import java.io.*;
 import java.util.LinkedList;
-import GUI.formatCse360SubClass;
+import GUI.Helper;
 
-public class formatCse360 {
+public class Formatter {
 	
-	public static class formatCse360Project
+	public static class FormatterOutput
 	{
-		public LinkedList<formatCse360SubClass> inputList;
+		public LinkedList<Helper> inputList;
 		public int linesRem;
 	}
 	
-	private static String input1;
-	private static formatCse360Project outputItem;
-	private static formatCse360SubClass newLine;
+	private static String inputline;
+	private static FormatterOutput outputItem;
+	private static Helper newLine;
 	private static String tempStr;
 	private static int x;
 	private static char[] tempInput;
 	private static char tempChar;
 	
-	public static formatCse360Project formatInput(String fileName) throws IOException
+	public static FormatterOutput formatInput(String fileName) throws IOException
 	{
 		
 		InputStream is = new FileInputStream(fileName);
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader stdin = new BufferedReader(isr);
 		
-		outputItem = new formatCse360Project();
+		outputItem = new FormatterOutput();
 		
-		outputItem.inputList = new LinkedList<formatCse360SubClass>();
+		outputItem.inputList = new LinkedList<Helper>();
 		x = 0;
 		tempChar = 'a';
 		tempStr = new String();
 		outputItem.linesRem = 0;
 		
-		input1 = stdin.readLine();
+		inputline = stdin.readLine();
 		
-		newLine = new formatCse360SubClass();
+		newLine = new Helper();
 		
 		do
 		{
-			tempInput = input1.toCharArray();
+			tempInput = inputline.toCharArray();
 			
-			if (x >= input1.length()) //case if line is just '\r' input1 length will be 0
+			if (x >= inputline.length()) //case if line is just '\r' input1 length will be 0
 			{
 				outputItem.linesRem++;
 			}
@@ -100,7 +100,7 @@ public class formatCse360 {
 				x = 0;
 			}
 			
-		}while ((input1 = stdin.readLine()) != null); //end of file
+		}while ((inputline = stdin.readLine()) != null); //end of file
 		
 		outputItem.inputList.add(newLine);
 		
@@ -119,7 +119,7 @@ public class formatCse360 {
 		else if ((newLine.getSize() + tempStr.length()) >= 81)
 		{
 			outputItem.inputList.add(newLine);
-			newLine = new formatCse360SubClass();
+			newLine = new Helper();
 			newLine.addWord(tempStr);
 		}
 		else
